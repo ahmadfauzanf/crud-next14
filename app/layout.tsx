a1fcba31b19ff19b542c3dbb1241d430afc68edd
ahmdy <fauzanahmadfadillah@gmail.com>
2024-09-07 11:17:47 +0700

    ubah title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 
 export const metadata = {
-  title: "Products",
+  title: "Aplikasi Products",
 };
 
 const ProductLayout = ({ children }: { children: React.ReactNode }) => {
